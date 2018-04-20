43c5fd6153eaf0e80da27cb52b2e956c28ed1705
Eniola Anthony <eniolaanthony@gmail.com>
2018-04-20 22:13:08 +0100

    Added Entity Framework

---
+++ BethanysPieShop/BethanysPieShop/Services/PieRepository.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Models;
+
+namespace BethanysPieShop.Services
+{
+    public class PieRepository : IPieRepository
+    {
+        private readonly AppDbContext _context;
+
+        public PieRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Pie> GetAllPie()
+        {
+            return _context.Pies.OrderBy(p => p.Name);         
+        }
+
+        public Pie GetPieById(int pieid)
+        {
+            return _context.Pies.FirstOrDefault(p => p.Id == pieid);
+        }
+    }
+}
