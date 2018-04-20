d3aeb82ad1b14fe62508b335309ec8e0cc9c07fe
Eniola Anthony <eniolaanthony@gmail.com>
2018-04-20 17:05:20 +0100

    First commit

---
+++ BethanysPieShop/BethanysPieShop/Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethanysPieShop.Services;
+using BethanysPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace BethanysPieShop.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly IPieRepository _pieRepository;
+
+        public HomeController(IPieRepository pieRepository)
+        {
+            _pieRepository = pieRepository;
+        }
+        // GET: /<controller>/
+        public IActionResult Index()
+        {
+            //ViewBag.Title = "Pie Overview";
+            var model = new HomeViewModel();
+            model.Pies = _pieRepository.GetAllPie();
+            model.Title = "Welcome to Bethany's Pie Shop";
+            return View(model);
+        }
+    }
+}
---
+++ BethanysPieShop/BethanysPieShop/Services/IPieRepository.cs
@@ -0,0 +1,14 @@
+﻿using BethanysPieShop.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BethanysPieShop.Services
+{
+    public interface IPieRepository
+    {
+        IEnumerable<Pie> GetAllPie();
+        Pie GetPieById(int pieid); 
+    }
+}
