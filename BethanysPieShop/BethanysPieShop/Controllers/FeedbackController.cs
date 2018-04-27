e05c3702d749266f88638f0d76066554e2297fd8
Eniola Anthony <eniolaanthony@gmail.com>
2018-04-27 14:00:06 +0100

    Added authorization with all codes complete

--- BethanysPieShop/BethanysPieShop/Controllers/FeedbackController.cs
+++ BethanysPieShop/BethanysPieShop/Controllers/FeedbackController.cs
@@ -4,12 +4,14 @@ using System.Linq;
 using System.Threading.Tasks;
 using BethanysPieShop.Models;
 using BethanysPieShop.Services;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
 namespace BethanysPieShop.Controllers
 {
+    [Authorize]
     public class FeedbackController : Controller
     {
         private readonly IFeedbackRepository _feedback;
